f76376ab071aa0e604d522c082a5139173c66277
VulgarBear <lucas@shisify.com>
2020-07-31 21:08:18 -0400

    update to cute command

--- src/commands/fun/cute.js
+++ src/commands/fun/cute.js
@@ -11,7 +11,7 @@ class CuteCommand extends Command {
       args: [
         {
           id: 'cuteType',
-          type: ['dog', 'fox', 'cat'],
+          type: ['dog', 'fox', 'cat', 'bird'],
           prompt: {
             start: 'What kind of cuteness are your looking for? (dog, cat, fox, etc.)',
             retry: 'Please select dog, cat, fox, etc.'
@@ -38,21 +38,30 @@ class CuteCommand extends Command {
     const dogURL = 'https://random.dog/doggos'
     const foxURL = 'https://randomfox.ca/floof/'
     const catURL = 'https://api.thecatapi.com/v1/images/search'
+    const birbURL = 'http://shibe.online/api/birds?&urls=true&httpsUrls=true'
 
-    // Fetch stores and result from Various APIs
+    // Fetch stores result from Various APIs
     if (cuteType === 'dog') {
       var res = await fetch(dogURL).then(res => res.json())
       if (!res[0]) return m.edit(`${ohNo} I couldn't find anycute dogs.`).then(msg.delete())
       var randomImage = res[Math.floor(Math.random() * res.length)]
       var cuteImage = 'https://random.dog/' + randomImage
+      var sourceAPI = 'random.dog'
     } else if (cuteType === 'fox') {
       var res = await fetch(foxURL).then(res => res.json())
       if (!res.image) return m.edit(`${ohNo} I couldn't find any cute foxes.`).then(msg.delete())
       var cuteImage = res.image
+      var sourceAPI = 'randomfox.ca'
     } else if (cuteType === 'cat') {
       var res = await fetch(catURL).then(res => res.json())
       if (!res[0].url) return m.edit(`${ohNo} I couldn't find anycute cats.`).then(msg.delete())
       var cuteImage = res[0].url
+      var sourceAPI = 'thecatapi.com'
+    } else if (cuteType === 'bird') {
+      var res = await fetch(birbURL).then(res => res.json())
+      if (!res[0]) return m.edit(`${ohNo} I couldn't find anycute birbss.`).then(msg.delete())
+      var cuteImage = res[0]
+      var sourceAPI = 'shibe.online'
     } else return m.edit(`${ohNo} Something went wrong!.`).then(msg.delete())
 
     // Build embed and send
@@ -61,7 +70,7 @@ class CuteCommand extends Command {
       .setURL(cuteImage)
       .setColor(process.env.EMBED)
       .setTimestamp()
-      .setFooter(`Requested by ${msg.author.tag} | Cute Animal APIs`, `${msg.author.displayAvatarURL()}`)
+      .setFooter(`Requested by ${msg.author.tag} | ${sourceAPI} API`, `${msg.author.displayAvatarURL()}`)
       .setImage(cuteImage)
 
     m.edit({ embed }).then(msg.delete())
