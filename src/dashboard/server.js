21a4afc3ebf08fd8a5371ef9aa38a8d37917bb3b
VulgarBear <lucas@shisify.com>
2020-10-25 04:25:25 -0400

    .

--- src/dashboard/server.js
+++ src/dashboard/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
 const Logger = require('../util/Logger')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.redner('./views/index.pug'))
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => { Logger.info(`Dashboard has started on ${port}`, { tag: 'Dashboard' }) })
